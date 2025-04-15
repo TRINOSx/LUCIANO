72108808eb7764ecd32d71b87e754c64a35b3fee
TRINOSx <140844387+TRINOSx@users.noreply.github.com>
2025-04-14 21:40:50 -0600

    Update and rename EsculietaAliada.jsx.txt to EsculietaAliada.jsx

---
+++ EscuelitaAliada/EsculietaAliada.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+export const EscuelitaAliada = () => {
+  return (
+    <div className="escuelita-aliada">
+      <div className="group-wrapper-10">
+        <div className="group-16">
+          <div className="overlap-group-22">
+            <img
+              className="image-15"
+              alt="Image"
+              src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/image-38@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
