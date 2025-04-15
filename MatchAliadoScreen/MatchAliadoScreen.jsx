b302fa071b407e831ea2b168dcf2004f28e84d0a
TRINOSx <140844387+TRINOSx@users.noreply.github.com>
2025-04-14 21:44:16 -0600

    Update and rename MatchAliadoScreen.jsx.txt to MatchAliadoScreen.jsx

---
+++ MatchAliadoScreen/MatchAliadoScreen.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export const MatchAliadoScreen = () => {
+  return (
+    <div className="match-aliado-screen">
+      <div className="match-aliado-2">
+        <div className="group-18">
+          <img
+            className="dashboard-2"
+            alt="Dashboard"
+            src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/dashboard-1@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
