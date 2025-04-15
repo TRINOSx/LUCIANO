1224f98315cb722b652b1fdcb70beddd37866729
TRINOSx <140844387+TRINOSx@users.noreply.github.com>
2025-04-14 21:42:13 -0600

    Update and rename Mapado.jsx.txt to Mapado.jsx

---
+++ Mapado/Mapado.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+import { SocialConnection } from "../../components/SocialConnection";
+import { VuesaxLinear } from "../../components/VuesaxLinear";
+import { FinanceSecurityIdCard1 } from "../../icons/FinanceSecurityIdCard1";
+import { SocialConnectionUser11 } from "../../icons/SocialConnectionUser11";
+import { VuesaxBoldHeart3 } from "../../icons/VuesaxBoldHeart3";
+import "./style.css";
+
+export const Mapado = () => {
+  return (
+    <div className="mapado">
+      <div className="group-wrapper-8">
+        <div className="group-12">
+          <div className="overlap-56">
+            <div className="mapa">
+              <div className="overlap-57">
+                <div className="overlap-58">
+                  <img
+                    className="image-12"
+                    alt="Image"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/image-23.png"
+                  />
+
+                  <div className="rectangle-19" />
+
+                  <img
+                    className="search-4"
+                    alt="Search"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/search.svg"
+                  />
+
+                  <SocialConnection />
+                  <div className="content">
+                    <div className="vuesax-bold-heart-3-wrapper">
+                      <VuesaxBoldHeart3 className="vuesax-bold-heart-3" />
+                    </div>
+
+                    <VuesaxLinear />
+                    <div className="overlap-group-21">
+                      <div className="group-13">
+                        <div className="frame-6">
+                          <div className="frame-7">
+                            <div className="text-wrapper-83">
+                              ESCUELITA DE PEPITA
+                            </div>
+                          </div>
+
+                          <div className="text-wrapper-84">
+                            comprar un nuevo laboratorio
+                          </div>
+
+                          <div className="rectangle-20" />
+
+                          <div className="text-wrapper-85">15%</div>
+
+                          <div className="frame-8">
+                            <div className="frame-9" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <img
+                        className="heart"
+                        alt="Heart"
+                        src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/heart@2x.png"
+                      />
+                    </div>
+                  </div>
+
+                  <img
+                    className="comment-3"
+                    alt="Comment"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/comment-2.svg"
+                  />
+
+                  <SocialConnectionUser11
+                    className="social-connection-user-9"
+                    color="#848484"
+                  />
+                  <FinanceSecurityIdCard1
+                    className="finance-security-id-card-4"
+                    color="#848484"
+                  />
+                  <img
+                    className="line-19"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/line-254.svg"
+                  />
+
+                  <img
+                    className="line-20"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/line-254.svg"
+                  />
+
+                  <img
+                    className="line-21"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/line-254.svg"
+                  />
+
+                  <img
+                    className="line-22"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/line-254.svg"
+                  />
+
+                  <img
+                    className="line-23"
+                    alt="Line"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/line-254.svg"
+                  />
+                </div>
+
+                <img
+                  className="notification-3"
+                  alt="Notification"
+                  src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/notification-2@2x.png"
+                />
+              </div>
+
+              <div className="overlap-59">
+                <div className="mexicanos-primero-wrapper">
+                  <div className="mexicanos-primero-2">
+                    Mexicanos Primero
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jalisco
+                  </div>
+                </div>
+
+                <div className="nav-menu-2">
+                  <div className="text-wrapper-86">Inicio</div>
+
+                  <div className="text-wrapper-87">Novedades</div>
+
+                  <div className="text-wrapper-88">Nosotros</div>
+
+                  <div className="text-wrapper-89">Contacto</div>
+
+                  <div className="group-14">
+                    <div className="text-wrapper-90">Legal</div>
+                  </div>
+                </div>
+
+                <div className="text-wrapper-91">Iniciar Sesion</div>
+
+                <button className="button-11">
+                  <div className="text-wrapper-92">Registrate</div>
+                </button>
+
+                <div className="overlap-60">
+                  <div className="rectangle-21" />
+
+                  <div className="ellipse-10" />
+
+                  <img
+                    className="image-13"
+                    alt="Image"
+                    src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f5f9e0a1ef88816a121b97/img/image-13.png"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <img
+              className="rectangle-22"
+              alt="Rectangle"
+              src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/rectangle-1-1@2x.png"
+            />
+
+            <div className="vuesax-bold-heart-1-wrapper">
+              <VuesaxBoldHeart3 className="vuesax-bold-heart-1" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
