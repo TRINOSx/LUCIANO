3a2c236ff3a19f4f1bfae320e165cc920967c0cc
TRINOSx <140844387+TRINOSx@users.noreply.github.com>
2025-04-14 21:32:38 -0600

    Update and rename Escuelita.jsx.txt to Escuelita.jsx

---
+++ Escuelita/Escuelita.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+export const Escuelita = () => {
+  return (
+    <div className="escuelita">
+      <div className="group-wrapper-6">
+        <div className="group-11">
+          <div className="image-wrapper">
+            <img
+              className="image-10"
+              alt="Image"
+              src="https://cdn.animaapp.com/projects/67f56811c32b493e71bc8ca0/releases/67f70d2c71d63f2aaae2edd1/img/image-38@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
